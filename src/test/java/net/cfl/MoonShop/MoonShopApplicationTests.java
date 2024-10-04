66efcee1bc1786b83ecafc1e68c3c244360a1f67
Danna <dannalubel29@gmail.com>
2024-10-04 10:35:06 -0300

    commit inicial

---
+++ src/test/java/net/cfl/MoonShop/MoonShopApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cfl.MoonShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
